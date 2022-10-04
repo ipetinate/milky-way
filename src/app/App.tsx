f0205fcac42ce1abf4a7109d254b9041fd91af66
Isac Petinate <idpetinate@gmail.com>
2022-10-04 15:10:24 -0300

    Installing React Query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import { Router } from 'router'
 import { Toaster } from 'react-hot-toast'
 import { AppShell } from 'components/AppShell'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/query-core'
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <AppShell>
-      <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <AppShell>
+        <Toaster />
 
-      <Router />
-    </AppShell>
+        <Router />
+      </AppShell>
+    </QueryClientProvider>
   )
 }
